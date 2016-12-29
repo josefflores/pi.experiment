5bd5e2f8c2db826d315322d8b791ac8afc724ded
Jose Flores <jose.flores.152@gmail.com>
2016-12-29 00:26:36 -0500

    fix ,

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var gpio = require('rpi-gpio');
 
-var on = (pin) => {
-    gpio.setup(pin, gpio.DIR_OUT, () => {
-        gpio.write(pin, true, function(err) {
+var on = (pin)=>{
+    gpio.setup(pin, gpio.DIR_OUT, ()=>{
+        gpio.write(pin, true, (err)=>{
             if (err) throw err;
         });
     });
 };
 
-var off = (pin) => {
-    gpio.setup(pin, gpio.DIR_OUT, () => {
-        gpio.write(pin, false function(err) {
+var off = (pin)=>{
+    gpio.setup(pin, gpio.DIR_OUT, ()=>{
+        gpio.write(pin, false, (err)=>{
             if (err) throw err;
         });
     });
