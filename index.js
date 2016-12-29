0b1db60c224fe6fbc929ab486698027b0e7bc5af
Jose Flores <jose.flores.152@gmail.com>
2016-12-29 14:01:21 -0500

    code cleanup

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function getRandomInt(min, max) {
 var pins = {
     r: new Gpio(22, 'out'),
     g: new Gpio(17, 'out'),
-    b: new Gpio(27, 'out')
+    b: new Gpio(4, 'out')
 };
 
 interval = setInterval(function() { //#C
