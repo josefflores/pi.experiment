8226e3525f171fd07e40b1b2b639380f9e3f13f2
Jose Flores <jose.flores.152@gmail.com>
2016-12-28 22:22:00 -0500

    5 seconds pin on test

--- rgblight/index.js
+++ rgblight/index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+import pin from 'pi-gpio';
+
+pin.open(16, 'output', (err)=>{
+    pin.write(16, 1, ()=>{
+        pin.close(16);
+    });
+});
+
+// reading the data on the pin i.e pin : 16
+// pin.open(16, 'output', (err)=>{
+//    pin.read(16, (err, value)=>{
+//         pin.close(16);
+//    });
+// });
+
