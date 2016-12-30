bf8f8c2aae13086978b5fd4036122ef97b8e4bf2
Jose Flores <jose.flores.152@gmail.com>
2016-12-29 20:49:33 -0500

    refactoring

--- RGB.js
+++ RGB.js
@@ -1,6 +1,7 @@
 // IMPORTS
 var onoff = require('onoff');
 var Pins = require('./Pins');
+var Pins = require('./Util');
 
 var Gpio = onoff.Gpio;
 
@@ -18,7 +19,7 @@ module.exports = function(pin_r, pin_g, pin_b){
     };
 
     function random() {
-        var mask = getRandomInt(0, 7);
+        var mask = Util.getRandomInt(0, 7);
         state(mask);
     };
 
