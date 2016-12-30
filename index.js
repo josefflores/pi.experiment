ad34101cb044e4cf035ae14eef1c30b144d991d9
Jose Flores <jose.flores.152@gmail.com>
2016-12-30 11:58:15 -0500

    experiment - button  e/ rgb

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var Gpio = require('onoff').Gpio,
-   // led = new Gpio(14, 'out'),
-    button = new Gpio(24, 'in', 'both');
+//  IMPORTS
+var Gpio = require('onoff').Gpio;
+
+var RGB = require('./js/RGB');
+
+//  VARIABLES
+
+var led = new RGB(27,17,4);
+var button = new Gpio(24, 'in', 'both');
+var i = 0;
+
+// FUNCTIONS
 
 button.watch(function (err, value) {
     if (err) {
         throw err;
     }
-
-    console.log('.');//led.writeSync(value);
+    led.state(4)
+    console.log(i++, '.', value);//led.writeSync(value);
 });
 
 // Kill power on exit
