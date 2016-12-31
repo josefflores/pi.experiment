808d34c783c6ef17cd8fca580eb57756e676b6c3
Jose Flores <jose.flores.152@gmail.com>
2016-12-30 23:21:27 -0500

    experiment - button class - add stats

--- js/Button.js
+++ js/Button.js
@@ -10,8 +10,12 @@ var Button = function (pin, toggle_state, on, off) {
         length = 10,
         start = 0,
         stats = {
-            history: [0],
+            consecutive: {
+                click: false,
+                press: false
+            },
             duration: 0,
+            history: [0],
             switch: false,
         };
 
@@ -22,7 +26,7 @@ var Button = function (pin, toggle_state, on, off) {
     };
 
     var toggle = function (value) {
-        if (stats.history[0] == 1 &&
+        if (stats.history[0] &&
             stats.history[1] == 0)
             stats.switch = !stats.switch;
     };
@@ -31,6 +35,25 @@ var Button = function (pin, toggle_state, on, off) {
 
         stats.history.unshift(value);
 
+        duration(value);
+        type(value);
+
+        stats.history = stats.history.slice(0, length); // keep only 10 readings
+
+        return stats;
+    };
+
+    var type = function(value){
+        if (value == 0 && duration <= 150){
+            ++stats.consecutive.click;
+            stats.consecutive.press = 0;
+        } else {
+            stats.consecutive.click = 0;
+            ++stats.consecutive.press;
+        }
+    }
+
+    var duration = function(value){
         //  Start clock if it has not been started
         if (value && stats.history[1] == 0) {
             start = new Date().getTime();
@@ -38,12 +61,7 @@ var Button = function (pin, toggle_state, on, off) {
         } else if (value && stats.history[1]) {
             stats.duration = (new Date().getTime()) - start;
         }
-
-        stats.history = stats.history.slice(0, length); // keep only 10 readings
-
-        return stats;
-    };
-
+    }
     //  WATCHER
 
     button.watch(function (err, value) {
