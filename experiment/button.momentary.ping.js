303339db5f6bb85353b77430876f8ad1de208022
Jose Flores <jose.flores.152@gmail.com>
2017-01-03 21:10:20 -0500

    delayed off ping

--- experiment/button.momentary.ping.js
+++ experiment/button.momentary.ping.js
@@ -18,10 +18,8 @@ var button = new Button(24, false,
     function(stats){
         led.state(0);
     }, function(stats){
-        led.state(0);
-
         ping.sys.probe('192.168.1.149', function(isAlive){
-        (isAlive ? led.state(2) : led.state(4)).state(0, 1000);
+            (isAlive ? led.state(2) : led.state(4)).state(0, 1000);
         });
     });
 
